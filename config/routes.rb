0d96e5de363bb0994f1a862e1ad9f2aa64b72384
kooojidevman <tkkjbiz@gmail.com>
2024-08-13 21:37:31 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
+
   devise_for :users
   root 'home#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tweets  # 追加
+  resources :users  # 追加
 end
