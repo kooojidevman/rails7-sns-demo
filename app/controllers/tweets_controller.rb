877e084e57b5d2863b7ac9653ffe8b02f00af2fc
kooojidevman <tkkjbiz@gmail.com>
2024-08-13 21:41:40 +0900

    Tweetのアクション実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,14 @@
 class TweetsController < ApplicationController
   def new
+    @tweet = Tweet.new # 新規投稿用の空のインスタンス
   end
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def create
